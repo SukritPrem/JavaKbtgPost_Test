6aacf13ccd6a4f6711611668ccf7320fc9c6a749
Sukrit pipitkul <Surit.pi@ku.th>
2024-02-26 21:04:23 +0700

    delete comment not use

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/PosttestApplication.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/PosttestApplication.java
@@ -8,16 +8,7 @@ public class PosttestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PosttestApplication.class, args);
-//		String[] mockPasswords = {"password1", "password2", "password3"};
-//
-//		// Password encoder
-//		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//
-//		// Hash and print mock passwords
-//		for (String password : mockPasswords) {
-//			String hashedPassword = passwordEncoder.encode(password);
-//			System.out.println("Mock Password: " + password + ", Hashed Password: " + hashedPassword);
-//		}
+
 	}
 
 
