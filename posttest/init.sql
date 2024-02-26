fc73a225cd6fc2281b549cba16e3d4b61f64f0c4
Sukrit pipitkul <Surit.pi@ku.th>
2024-02-26 21:05:38 +0700

    add comment

--- posttest/init.sql
+++ posttest/init.sql
@@ -40,7 +40,7 @@ CREATE TABLE user_ticket_store(
 -- Initial data
 INSERT INTO lottery(price,amount,ticket) VALUES('199', '10', '000001');
 INSERT INTO lottery(price,amount,ticket) VALUES('199', '10', '111111');
---password=password1
+--password=password1 //don't protect path user password1 it's mock.
 INSERT INTO user_profile(userId, roles, encoderPassword) VALUES('0123456789','USER','$2a$10$vfDwXoqEU2jQp84JITQO7OjYVarypOxwt74H60Zo4RX2NCbE/hWHa');
 --password=password2
 INSERT INTO user_profile(userid, roles, encoderPassword) VALUES ('9999999999','ADMIN','$2a$10$hSUyAUw9YKvUoyH4TZdRxOBfEqqTNlQJlStR570s2Krp9euM7kDPi');
@@ -50,7 +50,7 @@ INSERT INTO user_ticket(userId, roles, user_action, ticket, amount, price) VALUE
 --INSERT INTO user_ticket(userId, roles, user_action, ticket, amount, price) VALUES('0123456789', 'USER', 'BUY','000001','1','199');
 --INSERT INTO user_ticket_store(userId, ticket, amount, price) VALUES('0123456789','000001','1','199');
 
---password=password3
+--password=password3 //don't protect path user password3 it's mock.
 INSERT INTO user_profile(userid, roles, encoderPassword) VALUES ('0123456781','USER','$2a$10$gsWN3aRlWJMK1pl0rd/8KuPXI7pwWhA9VLD5rhjqARIdbauop9332');
 
 --INSERT INTO user_ticket(userId, roles, user_action, ticket, amount, price) VALUES('0123456781', 'USER', 'BUY','000001','1', '199');
